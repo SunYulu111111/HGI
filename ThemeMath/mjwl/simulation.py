068a9a599d2500f942e6c2bff374123ad08be973
SunYulu111111 <sunyulu@boke.com>
2026-06-22 18:16:29 +0800

    mjwl

--- ThemeMath/mjwl/simulation.py
+++ ThemeMath/mjwl/simulation.py
@@ -27,12 +27,12 @@ except ImportError:
 
 m = ThemeMath()
 
-SPIN_TIMES = 10000
+SPIN_TIMES = 100000
 INDEX = [8]
 GENERAL_INDEX = [1]
 REPORT_INTERVAL = 5000
 THRESHOLDS = (5, 10, 20, 50, 100, 1000)
-CHOOSE_INDEXES = [2]
+CHOOSE_INDEXES = [5]
 GENERAL_SECTION_RE = re.compile(r"^\s*\[GENERAL_(\d+)\]\s*$", re.MULTILINE)
 RESULT_CSV = Path(__file__).resolve().with_name("simulate_result.csv")
 RESULT_CSV_FIELDS = [
